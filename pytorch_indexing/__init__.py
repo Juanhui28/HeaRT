6ce4990fe3db31540a109dedf87609fc2fd47042
HarryShomer <harryshomer@gmail.com>
2023-06-20 19:58:16 -0400

    remove pytorch indexing in root

--- pytorch_indexing/__init__.py
+++
@@ -1,3 +0,0 @@
-from .compare_all_elements import compare_all_elements
-from .spspmm import spspmm
-from .testing import hello
--- pytorch_indexing/tests/test_testing.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-
-import pytorch_indexing
-from pytorch_indexing import testing
-
-class TestTesting(TestCase):
-    def test_is_string(self):
-        s = testing.hello()
-        self.assertTrue(isinstance(s, str))
